b3a0899049d839cb0eb966a592e8c7cff903918e
samuelmalik <samuelmalik9@gmail.com>
2022-09-26 21:26:52 +0200

    added function returning missing number

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._12
 {
@@ -6,7 +7,8 @@ namespace Open_Lab_04._12
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+
+            return 55 - nums.Sum();
         }
     }
 }
